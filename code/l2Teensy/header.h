a7f2da1cbe9a61fd3b761645a60c6930181e52e9
Vincent Cayadi <vincentcayadi45@gmail.com>
2022-06-28 17:02:28 +0800

    Added Some header files

---
+++ code/l2Teensy/header.h
@@ -0,0 +1,55 @@
+#include <HardwareSerial.h>
+#include <Arduino.h>
+
+// IR Sensors
+#define irOne 2 
+#define irTwo 3
+#define irThree 4
+#define irFour 10
+#define irFive 11
+#define irSix 45
+#define irSeven 46
+#define irEight 14
+#define irNine 15
+#define irTen 16
+#define irEleven 17
+#define irTwelve 18
+#define irThirteen 19
+#define irFourteen 20 
+#define irFifteen 21
+#define irSixteen 22
+#define irSeventeen 23
+#define irEighteen 24
+#define irNineteen 25
+#define irTwenty 26
+#define irTwentyOne 29
+#define irTwentyTwo 32
+#define irTwentyThree 33
+#define irTwentyFour 38
+
+//LED
+#define stmLED 39
+#define teensyLED  
+
+//PWM
+#define FLPWM 6
+#define FRPWM 9
+#define BLPWM 10
+#define BRPWM 11
+
+//DIR
+#define FLDIR 2
+#define FRDIR 3
+#define BLDIR 4
+#define BRDIR 5
+
+//Serial
+#define irSerial Serial1
+#define camSerial Serial2
+#define btSerial Serial3
+#define serialLayer4 Serial4
+#define serialLayer1 Serial5
+
+//Others
+#define ESC_LV 12
+#define SOL_LV 13
