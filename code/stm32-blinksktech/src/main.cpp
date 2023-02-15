79e09f151bdbf4f29829ed79ac09b456f3951ddc
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-02-15 21:52:52 +0800

    bless

--- code/stm32-blinksktech/src/main.cpp
+++ code/stm32-blinksktech/src/main.cpp
@@ -3,14 +3,14 @@
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(PC13, OUTPUT);
+  pinMode(PB15, OUTPUT);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly: 
-  digitalWrite(PC13, HIGH);
+  digitalWrite(PB15, HIGH);
   delay(250);
-  digitalWrite(PC13, LOW);
+  digitalWrite(PB15, LOW);
   delay(250);
 }
